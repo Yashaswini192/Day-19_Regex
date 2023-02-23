630e5e0ad7289efa4d3ffc7c7a52cb3ea517763a
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-23 21:08:04 +0530

    User need to enter a valid first name

---
+++ com/bl/regex/UserRegistration.java
@@ -0,0 +1,22 @@
+package com.bl.regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter FirstName:");
+		String firstName = s.nextLine();
+
+		if(Pattern.matches("^[A-Z]{1}[a-z]{2,}", firstName)) {
+			System.out.println("valid");
+		}else {
+			System.out.println("invalid");
+		}
+
+	}
+
+}
