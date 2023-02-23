96c77eeb58eb78fb24c01719dd6d6006fb7ae81e
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-23 21:02:51 +0530

    First commit

---
+++ com/bl/regex/Main.java
@@ -0,0 +1,13 @@
+package com.bl.regex;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("welcome to Regex");
+	}
+
+}
