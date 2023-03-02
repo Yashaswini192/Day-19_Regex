eb24a23e47f204efbdcbc9c5b037b95b5869f693
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-03-02 23:29:58 +0530

    Passed all sample emails

--- com/bl/regex/Main.java
+++ com/bl/regex/Main.java
@@ -8,6 +8,15 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		System.out.println("welcome to Regex");
+		
+		UserRegistration user = new  UserRegistration();
+		user.firstName();
+		user.secondName();
+		user.checkEmail();
+		user.phoneNumber();
+		user.password();
+		user.checkSampleEmail();
+	
 	}
 
 }
--- com/bl/regex/UserRegistration.java
+++ com/bl/regex/UserRegistration.java
@@ -1,63 +1,122 @@
 package com.bl.regex;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner s = new Scanner(System.in);
-		
+	Scanner s = new Scanner(System.in);
+
+	public void firstName() {
 		//UC1
 		System.out.println("Enter FirstName:");
 		String firstName = s.nextLine();
 
-		if(Pattern.matches("^[A-Z]{1}[a-z]{2,}", firstName)) {
-			System.out.println("valid");
-		}else {
-			System.out.println("invalid");
-		}
-
-		//UC2
-		System.out.println("Enter SecondName:");
-		String lastName = s.nextLine();
-
-		if(Pattern.matches("^[A-Z]{1}[a-z]{2,}", lastName)) {
-			System.out.println("valid");
-		}else {
-			System.out.println("invalid");
-		}
-		
-		//UC3
-		System.out.println("Enter Email:");
-		String email = s.nextLine();
-
-		if(Pattern.matches("^[A-z]+[a-zA-Z0-9]*[-._+/]?+[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$", email)){
-			System.out.println("valid");
-		}else {
-			System.out.println("invalid");
-		}
-		
-		//UC4
-		System.out.println("enter PhoneNumber:");
-		String phoneNumber = s.nextLine();
-		
-		if(Pattern.matches("^[0-91]{2}[\s]?[0-9]{10}", phoneNumber)) {
-			System.out.println("valid");
-		}else {
-			System.out.println("invalid");
-		}
-
-		//UC5-password
-		System.out.println("enter Password:");
-		String password = s.nextLine();
-		
-		if(Pattern.matches("((?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8})", password)) {
+		if(Pattern.matches("^[A-Z]{1}[a-z]{2,}$", firstName)) {
 			System.out.println("valid");
 		}else {
 			System.out.println("invalid");
 		}
 	}
 
-}
+		public void secondName() {
+			//UC2
+			System.out.println("Enter SecondName:");
+			String lastName = s.nextLine();
+
+			if(Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lastName)) {
+				System.out.println("valid");
+			}else {
+				System.out.println("invalid");
+			}
+		}
+
+			public void checkEmail() {
+				//UC3
+				System.out.println("Enter Email:");
+				String email = s.nextLine();
+
+				if(Pattern.matches("^[A-z]+[a-zA-Z0-9]*[-._+/]?+[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$", email)){
+					System.out.println("valid");
+				}else {
+					System.out.println("invalid");
+				}
+			}
+
+			public void phoneNumber() {
+				//UC4
+				System.out.println("enter PhoneNumber:");
+				String phoneNumber = s.nextLine();
+
+				if(Pattern.matches("^[0-91]{2}[\s]?[0-9]{10}$", phoneNumber)) {
+					System.out.println("valid");
+				}else {
+					System.out.println("invalid");
+				}
+			}
+
+			public void password() {
+				//UC5-password
+
+				System.out.println("enter Password:");
+				String password = s.nextLine();
+
+				if(Pattern.matches("((?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8}$)", password)) {
+					System.out.println("valid");
+				}else {
+					System.out.println("invalid");
+				}
+			}
+			
+			
+			public void checkSampleEmail() {
+				System.out.println("Checking Sample Emails ");
+				// array for emails
+				ArrayList<String> email = new ArrayList<>();
+				// valid EmailIDs
+				email.add("abc@yahoo.com");
+				email.add("abc-100@yahoo.com");
+				email.add("abc.100@yahoo.com");
+				email.add("abc111@abc.com");
+				email.add("abc-100@abc.net");
+				email.add("abc.100@abc.com.au");
+				email.add("abc@1.com");
+				email.add("abc@gmail.com.com");
+				email.add("abc+100@gmail.com");
+				// invalid EmailIDs
+				email.add("abc");
+				email.add("abc..");
+				email.add("abc..@gmail.com");
+				email.add("abc@abc@gmail.com");
+				email.add("abc@.com.my");
+				email.add("abc123@gmail.a");
+				email.add("abc123@.com");
+				email.add("abc123@.com.com");
+				email.add(".abc@abc.com");
+				email.add("abc()*@gmail.com");
+				email.add("abc..2002@gmail.com");
+				email.add("abc.@gmail.com");
+				email.add("abc@abc@gmail.com");
+				email.add("abc@gmail.com.1a");
+				email.add("abc..@gmail.com");
+				email.add("abc@gmail.com.aa.au");
+
+				String reGex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:([0-9]{1}|[a-zA-Z]{3,5})\\.)+[a-zA-Z]{2,3}";
+				// Creating/Defining a pattern to be searched
+				Pattern pattern = Pattern.compile(reGex);
+
+				for (String str : email) {
+					Matcher matcher = pattern.matcher(str);
+					if (matcher.matches()) {
+						System.out.println("valid email: " + str + " :" + matcher.matches());
+					} else {
+						System.out.println("Invalid email: " + str + " :" + matcher.matches());
+					}
+				}
+
+
+			}
+
+		}
